bb02f56abf2c5e6c06407fbf6369a146f599b205
李浩杰 <mo2635440982@gmail.com>
2026-03-23 11:43:04 +0800

    feat: 完成 Electron 至 Tauri v2 迁移

--- src-tauri/target/debug/deps/phf_shared-cadc60f38e03f1d9.d
+++
@@ -1,7 +0,0 @@
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\phf_shared-cadc60f38e03f1d9.d: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_shared-0.8.0\src\lib.rs
-
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\libphf_shared-cadc60f38e03f1d9.rlib: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_shared-0.8.0\src\lib.rs
-
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\libphf_shared-cadc60f38e03f1d9.rmeta: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_shared-0.8.0\src\lib.rs
-
-C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_shared-0.8.0\src\lib.rs:
--- src-tauri/target/debug/deps/smallvec-08881141c0b7ab82.d
+++
@@ -1,7 +0,0 @@
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\smallvec-08881141c0b7ab82.d: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\libsmallvec-08881141c0b7ab82.rlib: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-C:\Users\26354\Desktop\YouShouldDO\src-tauri\target\debug\deps\libsmallvec-08881141c0b7ab82.rmeta: C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-C:\Users\26354\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs:
